e9bc8a7c4145cd73f6130aa7b1819d79527db93b
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-12-27 07:50:38 -0500

    MVP Created (need to cut out more features fs)

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<nav class="fixed left-0 top-0 w-96 h-screen flex flex-col bg-red-50">
+    <a href="/exercise">Exercise</a>
+    <a href="/skills">Skills</a>
+    <a href="/missions">Missions</a>
+    <a href="/leaderboard">Leaderboard</a>
+    <a href="/profile">Profile</a>
+</nav>
+
+<div class="ml-96">
+    {@render children()}
+</div>
