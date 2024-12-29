315611a2d6f31457eadab59e805132476063fba5
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-12-29 02:23:13 -0500

    shop page

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,1 +1,34 @@
-Shop
+<main class="py-20 gap-16 flex flex-col justify-center items-center">
+    <h1 class="text-6xl text-center font-black tracking-tighter">
+        Some things to help
+        <span class="w-fit relative">
+            push you
+            <div class="w-full h-7 bg-blue-500/40 absolute bottom-2 right-1"></div>
+        </span>
+        along.
+    </h1>
+    <h2 class="w-1/2 text-2xl text-center">
+        Calisthenics is free, but a little equipment can make everything
+        a lot easier—even as a beginner.
+    </h2>
+    <div class="w-full px-20 gap-12 grid grid-cols-3 justify-center items-center">
+        <div class="w-full aspect-square bg-neutral-50 hover:bg-blue-100 rounded-3xl transition-colors duration-300">
+            <img alt="gymnastics rings"/>
+        </div>
+        <div class="w-full aspect-square bg-neutral-50 hover:bg-blue-100 rounded-3xl transition-colors duration-300">
+            <img alt="pullup bar"/>
+        </div>
+        <div class="w-full aspect-square bg-neutral-50 hover:bg-blue-100 rounded-3xl transition-colors duration-300">
+            <img alt="parallettes"/>
+        </div>
+        <div class="w-full aspect-square bg-neutral-50 hover:bg-blue-100 rounded-3xl transition-colors duration-300">
+            <img alt="creatine"/>
+        </div>
+        <div class="w-full aspect-square bg-neutral-50 hover:bg-blue-100 rounded-3xl transition-colors duration-300">
+            <img alt="fish oil"/>
+        </div>
+        <div class="w-full aspect-square bg-neutral-50 hover:bg-blue-100 rounded-3xl transition-colors duration-300">
+            <img alt="caffeine"/>
+        </div>
+    </div>
+</main>
