8cce064a31ed86c89b2e68efb88ecae84cd28ecf
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-12-29 03:56:12 -0500

    Header doesn't disappear

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@ import Header from '$lib/components/navigation/Header.svelte';
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </svelte:head>
 
-<div class="h-full flex flex-col">
+<div class="h-full flex flex-col overflow-auto">
     <Header />
     {@render children()}
     <Footer />
