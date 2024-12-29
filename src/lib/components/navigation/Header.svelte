583fe28d827c6bedb71f115491bf6b3098eb1cf8
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-12-29 03:17:30 -0500

    header updated to redirect to r/bwf

--- src/lib/components/navigation/Header.svelte
+++ src/lib/components/navigation/Header.svelte
@@ -13,7 +13,7 @@
 
         <div class="w-1/3 gap-4 flex justify-end items-center">
             <a href="/shop" class="border-[1px] px-6 py-[6px] flex justify-center items-center font-medium text-black  border-neutral-900 rounded-full">Shop</a>
-            <div class="px-6 py-[6px] flex justify-center items-center font-semibold text-white bg-blue-600 hover:bg-blue-700 rounded-full transition-colors duration-300 cursor-not-allowed">Join Now</div>
+            <a href="https://www.reddit.com/r/bodyweightfitness/" target="_blank" class="px-6 py-[6px] flex justify-center items-center font-semibold text-white bg-blue-600 hover:bg-blue-700 rounded-full transition-colors duration-300">Get Involved</a>
         </div>
     </nav>
 </header>
