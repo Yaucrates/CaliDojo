c39d61e8de3b8ccbd589785ea42f74d7e3308d63
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-12-28 22:38:36 -0500

    new design part 1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
+    import Header from '$lib/components/navigation/Header.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<title>CaliDojo | Gamify Your Calisthencis</title>
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100..900&display=swap" rel="stylesheet">
 </svelte:head>
 
+<Header />
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,9 @@
-<script>
-    import Header from "$lib/components/navigation/Header.svelte";
-</script>
-
 <svelte:head>
+    <title>CaliDojo | A Comprehensive Guide to Mastering Your Body</title>
 	<!-- <meta name="description" content="Impart is the best way of learning. All your classes are in one place accessible at anytime anywhere." /> -->
 </svelte:head>
 
 <main class="flex flex-col">
-    <Header />
-
     <div class="h-[50vh] bg-red-200"></div>
     <div class="h-[50vh] bg-green-200"></div>
     <div class="h-[50vh] bg-blue-200"></div>
