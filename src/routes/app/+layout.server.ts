f1614adac6eeac7068ab62af3d4e73cd427bd606
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-12-28 21:38:20 -0500

    Completed Streak Reset

--- src/routes/app/+layout.server.ts
+++ src/routes/app/+layout.server.ts
@@ -4,27 +4,32 @@ export const load: LayoutServerLoad = async ({ locals: { safeGetSession, supabas
   const { session } = await safeGetSession();
 
   // Update Streak to Zero if a Day is Missed
-  const today = new Date();
   const { data } = await supabase.from('user_data').select('*');
-  if (data) {
-    const lastStreakUpdate = new Date(data[0].last_streak_update);
-    const timeDifferenceMS = Math.abs(today.getTime() - lastStreakUpdate.getTime());
-    const timeDifferenceDays = timeDifferenceMS / (1000 * 60 * 60 * 24);
-    
-    if (timeDifferenceDays >= 1 && user) {
-        const year = today.getFullYear();
-        const month = String(today.getMonth() + 1).padStart(2, '0');
-        const day = String(today.getDate()).padStart(2, '0');
+  if (data && daySkipped(data[0].last_streak_update) && user) {
+    const today = new Date();
+    const year = today.getUTCFullYear();
+    const month = String(today.getUTCMonth() + 1).padStart(2, '0');
+    const day = String(today.getUTCDate()).padStart(2, '0');
 
-        await supabase.from("user_data").update({
-            last_streak_update: `${year}-${month}-${day}`,
-            streak: 0,
-        }).eq('user_id', user.id);
-    }
+    await supabase.from("user_data").update({
+        last_streak_update: `${year}-${month}-${day}`,
+        streak: 0,
+    }).eq('user_id', user.id);
   }
 
   return {
     session,
     cookies: cookies.getAll(),
   }
+}
+
+const daySkipped = (time: string) => {
+    const today = new Date();
+    const lastStreakUpdate = new Date(time);
+
+    const yearDiff = today.getFullYear() - lastStreakUpdate.getFullYear();
+    const monthDiff = today.getMonth() - lastStreakUpdate.getMonth();
+    const dayDiff = today.getDate() - lastStreakUpdate.getDate();
+    
+    return yearDiff > 0 || monthDiff > 0 || dayDiff > 1;
 }
