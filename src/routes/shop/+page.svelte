76b5ee98ea6fe55431e34ee2515acc6d6889789c
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-12-29 21:19:13 -0500

    Shop Hover Added

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -20,23 +20,47 @@
     <a id="shop" href="/shop/#shop" class="px-12 py-3 border-[1px] text-xl font-semibold rounded-full bg-blue-500/40 border-black">Check Them Out</a>
 
     <div class="w-full px-20 gap-12 grid grid-cols-3 justify-center items-center">
-        <div class="w-full aspect-square flex justify-center items-center bg-neutral-50 hover:bg-blue-100 rounded-3xl transition-colors duration-300">
+        <div class="w-full aspect-square flex justify-center items-center bg-neutral-50 rounded-3xl transition-colors duration-300 overflow-hidden group relative">
             <img src="/gymnastics-rings.webp" alt="gymnastics rings"/>
+
+            <div class="w-full h-full absolute top-0 left-0 flex justify-center items-center opacity-0 group-hover:opacity-100 bg-gradient-to-t from-neutral-300 to-transparent transition-opacity duration-300">
+                <div class="text-3xl font-black">Gymnastics Rings</div>
+            </div>
         </div>
-        <div class="w-full aspect-square flex justify-center items-center bg-neutral-50 hover:bg-blue-100 rounded-3xl transition-colors duration-300">
+        <div class="w-full aspect-square flex justify-center items-center bg-neutral-50 rounded-3xl transition-colors duration-300 overflow-hidden group relative">
             <img src="/doorway-pullup-bar.webp" alt="pullup bar"/>
+
+            <div class="w-full h-full absolute top-0 left-0 flex justify-center items-center opacity-0 group-hover:opacity-100 bg-gradient-to-t from-neutral-300 to-transparent transition-opacity duration-300">
+                <div class="text-3xl font-black">Pullup Bar</div>
+            </div>
         </div>
-        <div class="w-full aspect-square flex justify-center items-center bg-neutral-50 hover:bg-blue-100 rounded-3xl transition-colors duration-300">
+        <div class="w-full aspect-square flex justify-center items-center bg-neutral-50 rounded-3xl transition-colors duration-300 overflow-hidden group relative">
             <img src="/parallettes.webp" alt="parallettes" class="w-2/3"/>
+
+            <div class="w-full h-full absolute top-0 left-0 flex justify-center items-center opacity-0 group-hover:opacity-100 bg-gradient-to-t from-neutral-300 to-transparent transition-opacity duration-300">
+                <div class="text-3xl font-black">Parallettes</div>
+            </div>
         </div>
-        <div class="w-full aspect-square flex justify-center items-center bg-neutral-50 hover:bg-blue-100 rounded-3xl transition-colors duration-300">
-            <img src="/creatine.webp" alt="creatine" class="w-2/3"/>
+        <div class="w-full aspect-square flex justify-center items-center bg-neutral-50 rounded-3xl transition-colors duration-300 overflow-hidden group relative">
+            <img src="/creatine.webp" alt="creatine monohydrate" class="w-2/3"/>
+
+            <div class="w-full h-full absolute top-0 left-0 flex justify-center items-center opacity-0 group-hover:opacity-100 bg-gradient-to-t from-neutral-300 to-transparent transition-opacity duration-300">
+                <div class="text-3xl font-black">Creatine Monohydrate</div>
+            </div>
         </div>
-        <div class="w-full aspect-square flex justify-center items-center bg-neutral-50 hover:bg-blue-100 rounded-3xl transition-colors duration-300">
+        <div class="w-full aspect-square flex justify-center items-center bg-neutral-50 rounded-3xl transition-colors duration-300 overflow-hidden group relative">
             <img src="/fish-oil.webp" alt="fish oil" class="w-2/3"/>
+
+            <div class="w-full h-full absolute top-0 left-0 flex justify-center items-center opacity-0 group-hover:opacity-100 bg-gradient-to-t from-neutral-300 to-transparent transition-opacity duration-300">
+                <div class="text-3xl font-black">Fish Oil</div>
+            </div>
         </div>
-        <div class="w-full aspect-square flex justify-center items-center bg-neutral-50 hover:bg-blue-100 rounded-3xl transition-colors duration-300">
-            <img src="/caffeine.webp" alt="caffeine" class="w-3/4"/>
+        <div class="w-full aspect-square flex justify-center items-center bg-neutral-50 rounded-3xl transition-colors duration-300 overflow-hidden group relative">
+            <img src="/caffeine.webp" alt="Caffeine" class="w-3/4"/>
+
+            <div class="w-full h-full absolute top-0 left-0 flex justify-center items-center opacity-0 group-hover:opacity-100 bg-gradient-to-t from-neutral-300 to-transparent transition-opacity duration-300">
+                <div class="text-3xl font-black">Caffeine</div>
+            </div>
         </div>
     </div>
 </main>
