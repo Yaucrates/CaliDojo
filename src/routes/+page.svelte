b6846cca161fa3c5b8efa90acf07dc789056898f
Yaucrates <abdulhusseinyousifh@gmail.com>
2024-12-31 16:22:03 -0500

    Completed Main Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,14 @@
 	<meta name="description" content="CaliDojo is meant to provide with reliable resources on your fitness journey." />
 </svelte:head>
 
-<main class="h-[calc(100vh-72px)] flex">
-    <div class="pl-32 gap-6 w-1/2 flex flex-col justify-center">
-        <h1 class="text-6xl font-black tracking-tighter">
-            Your All·In·One Calisthenics Guide.
+<main class="h-[calc(100vh-72px)] flex justify-center items-center">
+    <div class="w-full md:w-3/4 lg:w-7/12 px-8 sm:px-12 gap-6 flex flex-col justify-center items-center">
+        <h1 class="text-5xl sm:text-6xl text-center font-black tracking-tighter">
+            Your All·In·One <br class="hidden lg:block" />Calisthenics Guide.
         </h1>
-        <h2 class="text-2xl">
+        <h2 class="text-2xl text-center">
             CaliDojo makes information about calisthenics freely and easily accessible all from one place.
         </h2>
-        <a href="/calisthenics" class="px-12 py-3 border-[1px] w-fit text-xl font-semibold rounded-full bg-blue-500/40 border-black">Get Started</a>
+        <a href="/calisthenics" class="px-12 py-3 border-[1px] w-fit text-xl text-center font-semibold rounded-full bg-blue-500/40 border-black">Get Started</a>
     </div>
 </main>
